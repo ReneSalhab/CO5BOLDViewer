722cfca71807eb220a80a378a7b3e5591e1b5f11
René Georg Salhab <grsalhab@hotmail.de>
2017-03-10 23:54:46 +0100

    Cleaned up uio.py
    - Put functions into corresponding classes.
    - Combined uio and uio_eos (uio can read .eos-files now)
    - uio_eos.py is obsolete and removed

--- uio.py
+++ uio.py
@@ -2,6 +2,10 @@
 Created on Tue Oct 29 2013
 
 @author: Kolja Glogowski
+
+Modified: René Salhab (10.03.2017)
+    - Put functions into corresponding classes
+    - Introduced compatibility with .eos files
 """
 
 import os
@@ -11,57 +15,6 @@ import numpy as np
 from struct import Struct   #, unpack
 
 _uint_from_bytes = Struct('>I')
-_re_desc = re.compile(r"^(\w+) (\w+) ?(.*)$")
-_re_params = re.compile(r"\w+=(?:(?:'[^']*')|(?:\S+))")
-_re_dims = re.compile('\d+:\d+')
-
-def _parse_descriptor(s):
-    m = _re_desc.match(s.decode())
-    if not m:
-        return None
-    etype, name, pstr = m.groups()
-    params = {}
-    pitems = _re_params.findall(pstr)
-    for it in pitems:
-        key, value = it.split('=', 1)
-        if value.startswith("'") and value.endswith("'"):
-            value = value[1:-1]
-        params[key] = value
-    return etype, name, params
-
-def _read_block_size(f):
-    data = f.read(4)
-    if len(data) != 4:
-        raise EOFError()
-    return _uint_from_bytes.unpack(data)[0]
-
-def _read_block(f):
-    nbytes = _read_block_size(f)
-    data = f.read(nbytes)
-    if nbytes != _read_block_size(f):
-        raise IOError('error reading block')
-    return data
-
-def _skip_block(f):
-    nbytes = _read_block_size(f)
-    f.seek(nbytes, 1)
-    if nbytes != _read_block_size(f):
-        raise IOError('error skipping block')
-
-def _read_string(f):
-    s = _read_block(f)
-    while s.rstrip().endswith(b'&'):
-        s = s.rstrip()[:-1] + _read_block(f)
-    return s.rstrip()
-
-def _read_array(f, dtype):
-    dt = np.dtype(dtype).newbyteorder('>')
-    nbytes = _read_block_size(f)
-    data = np.fromfile(f, dtype=dt, count=nbytes//dt.itemsize)
-    if nbytes != _read_block_size(f):
-        raise IOError('error reading array')
-    return data.astype(data.dtype.newbyteorder('='))
-
 
 class _EntryMapping(collections.Mapping):
     def __init__(self, entries):
@@ -106,7 +59,7 @@ class Entry(object):
         self.shape = shape
 
     def __repr__(self):
-        slist = [ self.type, self.name ]
+        slist = [self.type, self.name]
         for k in ['b', 'd', 'u']:
             if k in self.params:
                 v = self.params[k]
@@ -118,7 +71,7 @@ class Entry(object):
     @property
     def data(self):
         self._fd.seek(self.pos)
-        a = _read_array(self._fd, self.dtype)
+        a = self._read_array()
         if self.type != 'character':
             return a.reshape(self.shape) if self.shape != None else a[0]
         else:
@@ -128,6 +81,32 @@ class Entry(object):
                 return [s.rstrip() for s in a]
             else:
                 return a.reshape(self.shape)
+
+    def _read_array(self):
+        dt = np.dtype(self.dtype).newbyteorder('>')
+        nbytes = self._read_block_size()
+        data = np.fromfile(self._fd, dtype=dt, count=nbytes // dt.itemsize)
+        if nbytes != self._read_block_size():
+            raise IOError('error reading array')
+        return data.astype(data.dtype.newbyteorder('='))
+
+    def _read_block_size(self):
+        data = self._fd.read(4)
+        if len(data) != 4:
+            raise EOFError()
+        return _uint_from_bytes.unpack(data)[0]
+
+
+class Block(_EntryMapping):
+    def __init__(self, pos, params, entries):
+        self.pos = pos
+        self.params = params
+        super(Block, self).__init__(entries)
+
+    def __repr__(self):
+        slist = [ 'block' ]
+        slist.append('entries=%d' % len(self._entries))
+        return '<%s>' % (' '.join(slist))
 
 
 class Box(_EntryMapping):
@@ -155,105 +134,34 @@ class DataSet(_EntryMapping):
         slist.append('boxes=%d' % len(self.box))
         return '<%s>' % (' '.join(slist))
 
-
-def _read_file_header(f):
-    header_string = _read_string(f)
-    ff, uio, header = _parse_descriptor(header_string)
-    if ff != 'fileform' or uio != 'uio':
-        raise IOError('unknown file format')
-    return header
-
-def _read_entry(f):
-    etype, name, params = _parse_descriptor(_read_string(f))
-    if etype == 'table':
-        raise IOError('uio files containing tables are not supported')
-    pos = f.tell()
-    dtype = None
-    if 'b' in params:
-        nbytes = int(params['b'])
-        if etype == 'real':
-            dtype = 'f%d' % nbytes
-        elif etype == 'integer':
-            dtype = 'i%d' % nbytes
-        elif etype == 'character':
-            dtype = 'S%d' % nbytes
-        elif etype == 'complex':
-            dtype = 'c%d' % nbytes
-    shape = None
-    if 'd' in params:
-        fshape = []
-        for x in _re_dims.findall(params['d']):
-            x1, x2 = x.split(':')
-            fshape.append(int(x2)-int(x1)+1)
-        shape = tuple(reversed(fshape))
-    return Entry(fd=f, pos=pos, type=etype, name=name, params=params,
-                 dtype=dtype, shape=shape)
-
-def _read_box_entries(f):
-    box = []
-    while True:
-        entry = _read_entry(f)
-        if entry.type == 'label' and entry.name == 'endbox':
-            break
-        box.append(entry)
-        _skip_block(f)
-    return box
-
-def _read_dataset_entries(f):
-    entries = []
-    while True:
-        entry = _read_entry(f)
-        if entry.type == 'label':
-            if entry.name == 'enddataset':
-                break
-            elif entry.name == 'box':
-                box = Box(pos=f.tell(), params=entry.params,
-                          entries=_read_box_entries(f))
-                entries.append(box)
-        else:
-            entries.append(entry)
-            _skip_block(f)
-    return entries
-
-def _read_file_entries(f):
-    entries = []
-    while True:
-        try:
-            entry = _read_entry(f)
-        except EOFError:
-            break
-        if entry.type == 'label' and entry.name == 'dataset':
-            ds_box = []
-            ds_entries = []
-            for ei in _read_dataset_entries(f):
-                if isinstance(ei, Box):
-                    ds_box.append(ei)
-                elif isinstance(ei, Entry):
-                    ds_entries.append(ei)
-            ds = DataSet(pos=f.tell(), params=entry.params,
-                         entries=ds_entries, box=ds_box)
-            entries.append(ds)
-        else:
-            entries.append(entry)
-            _skip_block(f)
-    return entries
-
-
 class File(_EntryMapping):
     def __init__(self, filename):
+        _, fend = os.path.splitext(filename)
         self._fd = open(os.path.abspath(filename), 'rb')
+
+        self._re_params = re.compile(r"\w+=(?:(?:'[^']*')|(?:\S+))")
+        self._re_desc = re.compile(r"^(\w+) (\w+) ?(.*)$")
+        self._re_dims = re.compile('\d+:\d+')
 
         mag = self._fd.read(4+12)[4:]
         if mag != b'fileform uio':
             raise IOError('unsupported file format')
         self._fd.seek(0)
-        self.header = _read_file_header(self._fd)
+        ff, uio, self.header = self._parse_descriptor()
+        if ff != 'fileform' or uio != 'uio':
+            raise IOError('unknown file format')
 
         entries = []
-        self.dataset = []
-        for x in _read_file_entries(self._fd):
+        if fend == ".eos":
+            self.block = []
+        else:
+            self.dataset = []
+
+        for x in self._read_file_entries():
             if isinstance(x, DataSet):
                 self.dataset.append(x)
+            elif isinstance(x, Block):
+                self.block.append(x)
             elif isinstance(x, Entry):
                 entries.append(x)
         super(File, self).__init__(entries)
@@ -269,7 +177,126 @@ class File(_EntryMapping):
     def name(self):
         return self._fd.name
 
+    def _read_file_entries(self):
+        entries = []
+        while True:
+            try:
+                entry = self._read_entry()
+            except EOFError:
+                break
+            if entry.type == 'label' and entry.name == 'dataset':
+                ds_box = []
+                ds_entries = []
+                for ei in self._read_dataset_entries():
+                    if isinstance(ei, Box):
+                        ds_box.append(ei)
+                    elif isinstance(ei, Entry):
+                        ds_entries.append(ei)
+                ds = DataSet(pos=self._fd.tell(), params=entry.params, entries=ds_entries, box=ds_box)
+                entries.append(ds)
+            elif entry.type == 'label' and entry.name == 'block':
+                ds_entries = []
+                for ei in self._read_box_entries():
+                    if isinstance(ei, Entry):
+                        ds_entries.append(ei)
+                ds = Block(pos=self._fd.tell(), params=entry.params, entries=ds_entries)
+                entries.append(ds)
+            else:
+                entries.append(entry)
+                self._skip_block()
+        return entries
 
+    def _read_dataset_entries(self):
+        entries = []
+        while True:
+            entry = self._read_entry()
+            if entry.type == 'label':
+                if entry.name == 'enddataset':
+                    break
+                elif entry.name == 'box':
+                    box = Box(pos=self._fd.tell(), params=entry.params, entries=self._read_box_entries())
+                    entries.append(box)
+                elif entry.name == 'block':
+                    box = Block(pos=self._fd.tell(), params=entry.params, entries=self._read_box_entries())
+                    entries.append(box)
+            else:
+                entries.append(entry)
+                self._skip_block()
+        return entries
+
+    def _read_box_entries(self):
+        box = []
+        while True:
+            entry = self._read_entry()
+            if entry.type == 'label' and (entry.name == 'endbox' or entry.name == 'endblock'):
+                break
+            box.append(entry)
+            self._skip_block()
+        return box
+
+    def _read_entry(self):
+        etype, name, params = self._parse_descriptor()
+        if etype == 'table':
+            raise IOError('uio files containing tables are not supported')
+        pos = self._fd.tell()
+        dtype = None
+        if 'b' in params:
+            nbytes = int(params['b'])
+            if etype == 'real':
+                dtype = 'f%d' % nbytes
+            elif etype == 'integer':
+                dtype = 'i%d' % nbytes
+            elif etype == 'character':
+                dtype = 'S%d' % nbytes
+            elif etype == 'complex':
+                dtype = 'c%d' % nbytes
+        shape = None
+        if 'd' in params:
+            fshape = []
+            for x in self._re_dims.findall(params['d']):
+                x1, x2 = x.split(':')
+                fshape.append(int(x2) - int(x1) + 1)
+            shape = tuple(reversed(fshape))
+        return Entry(fd=self._fd, pos=pos, type=etype, name=name, params=params, dtype=dtype, shape=shape)
+
+    def _read_string(self):
+        s = self._read_block()
+        while s.rstrip().endswith(b'&'):
+            s = s.rstrip()[:-1] + self._read_block()
+        return s.rstrip()
+
+    def _read_block(self):
+        nbytes = self._read_block_size()
+        data = self._fd.read(nbytes)
+        if nbytes != self._read_block_size():
+            raise IOError('error reading block')
+        return data
+
+    def _skip_block(self):
+        nbytes = self._read_block_size()
+        self._fd.seek(nbytes, 1)
+        if nbytes != self._read_block_size():
+            raise IOError('error skipping block')
+
+    def _read_block_size(self):
+        data = self._fd.read(4)
+        if len(data) != 4:
+            raise EOFError()
+        return _uint_from_bytes.unpack(data)[0]
+
+    def _parse_descriptor(self):
+        m = self._re_desc.match(self._read_string().decode())
+        if not m:
+            return None
+        etype, name, pstr = m.groups()
+        params = {}
+        pitems = self._re_params.findall(pstr)
+        for it in pitems:
+            key, value = it.split('=', 1)
+            if value.startswith("'") and value.endswith("'"):
+                value = value[1:-1]
+            params[key] = value
+        return etype, name, params
 
 if __name__ == '__main__':
     from numpy import *
